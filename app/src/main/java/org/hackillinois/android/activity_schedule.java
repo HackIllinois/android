a41a0c1983e07022a7aec348dc052795274bdab4
kelley <kelleyc2@illinois.edu>
2018-11-14 00:12:24 -0600

    Implemented recyclerview without data

--- app/src/main/java/org/hackillinois/android/activity_schedule.java
+++ app/src/main/java/org/hackillinois/android/activity_schedule.java
@@ -1,9 +1,12 @@
 package org.hackillinois.android;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -17,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class activity_schedule extends AppCompatActivity {
@@ -35,6 +39,8 @@ public class activity_schedule extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +62,16 @@ public class activity_schedule extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // TODO: get rid of floating action button
 
+        // TODO: add recycler view
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.activity_schedule_recyclerview);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(dataset);
+        recyclerView.setAdapter(mAdapter);
     }
 
 
@@ -93,23 +100,25 @@ public class activity_schedule extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DayFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
+        public DayFragment() {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static DayFragment newInstance(int sectionNumber) {
+            DayFragment fragment = new DayFragment();
             Bundle args = new Bundle();
+
+            // Depending on the section number, filter out dates from the JSON
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
             return fragment;
@@ -139,13 +148,73 @@ public class activity_schedule extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return DayFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
             return 3;
+        }
+    }
+
+    // Adapter for RecyclerView
+    // TODO: adapt to schedule view
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            TextView tv_eventTitle;
+            TextView tv_eventDetail;
+            TextView tv_eventLocation;
+            ImageButton imageButton_star;
+            ConstraintLayout constraintLayout_recyclerView;
+
+            public MyViewHolder(View parent) {
+                super(parent);
+                tv_eventTitle = itemView.findViewById(R.id.eventTitle);
+                tv_eventDetail = itemView.findViewById(R.id.eventDetail);
+                tv_eventLocation = itemView.findViewById(R.id.eventLocation);
+                imageButton_star = itemView.findViewById(R.id.star);
+                constraintLayout_recyclerView = itemView.findViewById(R.id.constraintLayout);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        // TODO: get JSON data for this
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_events, parent, false);
+            MyAdapter.MyViewHolder viewHolder = new MyAdapter.MyViewHolder(view);
+            return viewHolder;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.tv_eventTitle.setText();
+            holder.tv_eventDetail.setText();
+            holder.tv_eventLocation.setText();
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
         }
     }
 }
