ed4a9061a0def9856f50bfebc1ed58caa9f1ac65
patrickfeltes <pj6444@gmail.com>
2018-12-05 21:34:29 -0600

    Add EventInfoActivity and link to google maps

--- app/src/main/java/org/hackillinois/android/model/Event.kt
+++ app/src/main/java/org/hackillinois/android/model/Event.kt
@@ -1,9 +1,50 @@
 package org.hackillinois.android.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Event(
     val name: String,
     val description: String,
     val startTime: Long,
     val endTime: Long,
-    val locationDescription: String
-)
+    val locationDescription: String,
+    val latitude: Double,
+    val longitude: Double
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readLong(),
+            parcel.readLong(),
+            parcel.readString(),
+            parcel.readDouble(),
+            parcel.readDouble())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.apply {
+            writeString(name)
+            writeString(description)
+            writeLong(startTime)
+            writeLong(endTime)
+            writeString(locationDescription)
+            writeDouble(latitude)
+            writeDouble(longitude)
+        }
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Event> {
+        override fun createFromParcel(parcel: Parcel): Event {
+            return Event(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Event?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
