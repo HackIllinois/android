1574dc99d40a82a0ed5212dc49b03259d3470cf3
patrickfeltes <pj6444@gmail.com>
2018-12-26 17:44:26 -0600

    Small cleanup

--- app/src/main/java/org/hackillinois/android/database/entity/Event.kt
+++ app/src/main/java/org/hackillinois/android/database/entity/Event.kt
@@ -2,7 +2,6 @@ package org.hackillinois.android.database.entity
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
-import java.util.*
 
 @Entity(tableName = "events")
 data class Event(
@@ -13,9 +12,4 @@ data class Event(
     val locationDescription: String,
     val latitude: Double,
     val longitude: Double
-) : BaseEntity() {
-    fun getStart(): Calendar = Calendar.getInstance().apply { timeInMillis = startTime * MILLIS_IN_SECONDS }
-    fun getEnd(): Calendar = Calendar.getInstance().apply { timeInMillis = endTime * MILLIS_IN_SECONDS }
-}
-
-private const val MILLIS_IN_SECONDS = 1000L
+) : BaseEntity()
