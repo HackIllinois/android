28eb5d2064fd5d3e45758b3110419a5df0cdc09c
kelley <kelleyc2@illinois.edu>
2019-01-12 17:23:25 -0500

    Removed an unused import

--- app/src/main/java/org/hackillinois/android/view/schedule/ScheduleFragment.java
+++ app/src/main/java/org/hackillinois/android/view/schedule/ScheduleFragment.java
@@ -13,7 +13,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
