c7e06d86f8c376b6391ac8570eac244b2e95c6fc
kelley <kelleyc2@illinois.edu>
2019-01-16 21:53:49 -0600

    Set schedule timezone

--- app/src/main/java/org/hackillinois/android/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/org/hackillinois/android/viewmodel/ScheduleViewModel.kt
@@ -2,15 +2,25 @@ package org.hackillinois.android.viewmodel
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
-import java.sql.Timestamp
 import org.hackillinois.android.repository.EventRepository
+import java.util.TimeZone
+import java.util.Calendar
 
 class ScheduleViewModel : ViewModel() {
     private val eventRepository = EventRepository.instance
 
-    val FRIDAY_END = Timestamp.valueOf("2019-02-23 00:00:00").time
-    val SATURDAY_END = Timestamp.valueOf("2019-02-24 00:00:00").time
-    val SUNDAY_END = Timestamp.valueOf("2019-02-25 00:00:00").time
+    private val FRIDAY_END = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        set(2019, Calendar.FEBRUARY, 23, 0, 0, 0)
+    }.timeInMillis
+    private val SATURDAY_END = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        set(2019, Calendar.FEBRUARY, 24, 0, 0, 0)
+    }.timeInMillis
+    private val SUNDAY_END = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        set(2019, Calendar.FEBRUARY, 25, 0, 0, 0)
+    }.timeInMillis
 
     lateinit var fridayEventsLiveData: LiveData<List<org.hackillinois.android.database.entity.Event>>
     lateinit var saturdayEventsLiveData: LiveData<List<org.hackillinois.android.database.entity.Event>>
