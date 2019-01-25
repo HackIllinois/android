1286a14f2a2bf5a26071588f33a108fd3b1fdbb8
patrickfeltes <pj6444@gmail.com>
2019-01-25 16:51:39 -0600

    Notify from firebase while in app and fix notification timing bug

--- app/src/main/java/org/hackillinois/android/firebase/FirebaseNotificationMessagingService.kt
+++ app/src/main/java/org/hackillinois/android/firebase/FirebaseNotificationMessagingService.kt
@@ -1,11 +1,20 @@
 package org.hackillinois.android.firebase
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import org.hackillinois.android.App
+import org.hackillinois.android.common.HackIllinoisNotificationManager
 import kotlin.concurrent.thread
 
 class FirebaseNotificationMessagingService: FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         thread { App.getAPI().sendUserToken(DeviceToken(token)).execute() }
+    }
+
+    // this function is only called when the app is in the foreground and receives a notification
+    override fun onMessageReceived(message: RemoteMessage?) {
+        val title = message?.notification?.title ?: "Empty Title"
+        val body = message?.notification?.body ?: "Empty Body"
+        HackIllinoisNotificationManager.runFirebaseInAppNotification(this, title, body)
     }
 }
