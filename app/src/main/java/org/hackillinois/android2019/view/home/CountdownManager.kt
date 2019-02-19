4bfcce75a22e32ea3d5947e4738522166971975f
Brian Strauch <bstrauch24@gmail.com>
2019-02-18 18:05:24 -0600

    Sync event times with iOS

--- app/src/main/java/org/hackillinois/android2019/view/home/CountdownManager.kt
+++ app/src/main/java/org/hackillinois/android2019/view/home/CountdownManager.kt
@@ -9,7 +9,7 @@ class CountdownManager(val listener: CountDownListener) {
 
     private val eventStartTime: Calendar = Calendar.getInstance().apply {
         timeZone = TimeZone.getTimeZone("America/Chicago")
-        set(2019, Calendar.FEBRUARY, 22, 17, 0, 0)
+        set(2019, Calendar.FEBRUARY, 22, 16, 0, 0)
     }
 
     private val hackingStartTime: Calendar = Calendar.getInstance().apply {
@@ -19,7 +19,7 @@ class CountdownManager(val listener: CountDownListener) {
 
     private val hackingEndTime: Calendar = Calendar.getInstance().apply {
         timeZone = TimeZone.getTimeZone("America/Chicago")
-        set(2019, Calendar.FEBRUARY, 24, 11, 0, 0)
+        set(2019, Calendar.FEBRUARY, 24, 10, 0, 0)
     }
 
     private val times = listOf(eventStartTime, hackingStartTime, hackingEndTime)
