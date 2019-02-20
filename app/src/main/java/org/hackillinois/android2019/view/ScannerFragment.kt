8999de59a6343fc022b976fd44b10ebdc304f003
Pradyumna Shome <pradyumna.shome@gmail.com>
2019-02-20 16:33:30 -0600

    Adds Check In "event" to scan list.

--- app/src/main/java/org/hackillinois/android2019/view/ScannerFragment.kt
+++ app/src/main/java/org/hackillinois/android2019/view/ScannerFragment.kt
@@ -175,7 +175,11 @@ class ScannerFragment : Fragment() {
      * @param eventList the list of events from the latest publish event (local DB / API call)
      */
     private fun updateEventList(eventList: List<Event>?) = eventList.let { eventList ->
-        var eventNameList: List<String> = eventList!!.map { it.name }
+        var eventNameList: MutableList<String> = eventList!!.map { it.name }.toMutableList()
+        // The special element in the list that allows check in to the event, as opposed to
+        // event check-in.
+        eventNameList.add(CHECK_IN_TEXT)
+
         var spinnerAdapter: ArrayAdapter<String> = ArrayAdapter(context,
                 R.layout.simple_dropdown_item_1line, eventNameList)
 
