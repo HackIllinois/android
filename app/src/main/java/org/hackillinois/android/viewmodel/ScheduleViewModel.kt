57bf3563a0bcbbf0e7fd5529de3339b91950dc7f
patrickfeltes <pj6444@gmail.com>
2019-10-09 21:31:02 -0500

    Change day end times to 2020 times

--- app/src/main/java/org/hackillinois/android/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/org/hackillinois/android/viewmodel/ScheduleViewModel.kt
@@ -10,17 +10,17 @@ class ScheduleViewModel : ViewModel() {
 
     val fridayEnd = Calendar.getInstance().apply {
         timeZone = TimeZone.getTimeZone("America/Chicago")
-        set(2019, Calendar.FEBRUARY, 23, 0, 0, 0)
+        set(2020, Calendar.FEBRUARY, 29, 0, 0, 0)
     }.timeInMillis
 
     val saturdayEnd = Calendar.getInstance().apply {
         timeZone = TimeZone.getTimeZone("America/Chicago")
-        set(2019, Calendar.FEBRUARY, 24, 0, 0, 0)
+        set(2020, Calendar.MARCH, 1, 0, 0, 0)
     }.timeInMillis
 
     val sundayEnd = Calendar.getInstance().apply {
         timeZone = TimeZone.getTimeZone("America/Chicago")
-        set(2019, Calendar.FEBRUARY, 25, 0, 0, 0)
+        set(2020, Calendar.FEBRUARY, 2, 0, 0, 0)
     }.timeInMillis
 
     lateinit var fridayEventsLiveData: LiveData<List<org.hackillinois.android.database.entity.Event>>
