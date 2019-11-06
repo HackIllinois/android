ad895373a20185fe85afe08c4cd611ec3d284a79
patrickfeltes <pj6444@gmail.com>
2019-11-06 15:42:26 -0600

    Create UI for bottom app bar

--- app/src/main/java/org/hackillinois/android/view/MainActivity.kt
+++ app/src/main/java/org/hackillinois/android/view/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.core.view.GravityCompat
 import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.layout_nav_menu.*
 import kotlinx.android.synthetic.main.layout_nav_menu.view.*
 import org.hackillinois.android.App
@@ -26,129 +28,150 @@ import org.hackillinois.android.view.schedule.ScheduleFragment
 import org.hackillinois.android.viewmodel.MainViewModel
 import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private val defaultToken: String = ""
-
-    private lateinit var navViews: List<View>
-    private lateinit var viewModel: MainViewModel
+//
+//    private lateinit var navViews: List<View>
+//    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(toolbar)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu)
-            title = "HOME"
-        }
+        val selectedIconColor = ContextCompat.getColor(this, R.color.selectedAppBarIcon)
+        val unselectedIconColor = ContextCompat.getColor(this, R.color.unselectedAppBarIcon)
 
-        val startFragment = HomeFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.contentFrame, startFragment).commit()
+        val bottomBarButtons = listOf(
+            bottomAppBar.homeButton,
+            bottomAppBar.todayButton,
+            bottomAppBar.mapButton,
+            bottomAppBar.groupButton
+        )
 
-        navViews = listOf(navHome, navSchedule, navOutdoorMaps, navIndoorMaps, navProfile, navLogout, navAdmin, navScanner)
-        navViews.forEach { it.setOnClickListener(this) }
-
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.init()
-        viewModel.user.observe(this, Observer { updateUserInfo(it) })
-        viewModel.roles.observe(this, Observer { updateRoles(it) })
-
-        updateDeviceToken()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                drawerLayout.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onClick(v: View) {
-        navViews.forEach { it.setBackgroundColor(Color.WHITE) }
-
-        if (v == navLogout) {
-            logout()
-            return
-        }
-
-        val fragment = when (v) {
-            navHome -> HomeFragment()
-            navSchedule -> ScheduleFragment()
-            navOutdoorMaps -> OutdoorMapsFragment()
-            navIndoorMaps -> IndoorMapsFragment()
-            navProfile -> ProfileFragment()
-            navAdmin -> AdminFragment()
-            navScanner -> ScannerFragment()
-            else -> return
-        }
-
-        v.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.selectedMenuItem))
-        supportActionBar?.title = (v as TextView).text
-
-        supportFragmentManager.beginTransaction().replace(R.id.contentFrame, fragment).commit()
-        drawerLayout.closeDrawer(GravityCompat.START)
-    }
-
-    private fun updateUserInfo(user: User?) {
-        user?.let {
-            navMenu.nameTextView.text = it.fullName
-            navMenu.emailTextView.text = it.email
-        }
-    }
-
-    /**
-     * Modify the available options in the nav menu
-     * Staff: Can access the scanner
-     * Admins: Can access scanner and admin tools
-     */
-    private fun updateRoles(roles: Roles?) = roles?.let { roles ->
-        var listOfRoles: List<String> = roles.roles
-        if (listOfRoles.contains("Admin")) {
-            navAdmin.visibility = View.VISIBLE
-            navScanner.visibility = View.VISIBLE
-        }
-        if (listOfRoles.contains("Staff")) {
-            navScanner.visibility = View.VISIBLE
-        }
-    }
-
-    private fun logout() {
-        clearJWT()
-
-        thread {
-            App.database.clearAllTables()
-
-            runOnUiThread {
-                val loginIntent = Intent(this, LoginActivity::class.java)
-                loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(loginIntent)
-                finish()
+        // by default, home button is selected
+        bottomAppBar.homeButton.setColorFilter(selectedIconColor)
+        
+        // make all buttons unselectedColor and then set selected button to selectedColor
+        bottomBarButtons.forEach { button ->
+            button.setOnClickListener { view ->
+                bottomBarButtons.forEach { (it as ImageButton).setColorFilter(unselectedIconColor) }
+                (view as ImageButton).setColorFilter(selectedIconColor)
             }
         }
+
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.apply {
+//            setDisplayHomeAsUpEnabled(true)
+//            setHomeAsUpIndicator(R.drawable.ic_menu)
+//            title = "HOME"
+//        }
+//
+//        val startFragment = HomeFragment()
+//        supportFragmentManager.beginTransaction().replace(R.id.contentFrame, startFragment).commit()
+//
+//        navViews = listOf(navHome, navSchedule, navOutdoorMaps, navIndoorMaps, navProfile, navLogout, navAdmin, navScanner)
+//        navViews.forEach { it.setOnClickListener(this) }
+//
+//        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+//        viewModel.init()
+//        viewModel.user.observe(this, Observer { updateUserInfo(it) })
+//        viewModel.roles.observe(this, Observer { updateRoles(it) })
+//
+//        updateDeviceToken()
     }
 
-    private fun clearJWT() {
-        val editor = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).edit()
-        editor.putString("jwt", defaultToken)
-        editor.apply()
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            android.R.id.home -> {
+//                drawerLayout.openDrawer(GravityCompat.START)
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 
-    private fun updateDeviceToken() {
-        val token = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).getString("firebaseToken", defaultToken)
-                ?: defaultToken
-        if (token != defaultToken) {
-            thread {
-                App.getAPI().sendUserToken(DeviceToken(token)).execute()
-
-                val editor = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).edit()
-                editor.putString("firebaseToken", defaultToken)
-                editor.apply()
-            }
-        }
-    }
+//    override fun onClick(v: View) {
+//        navViews.forEach { it.setBackgroundColor(Color.WHITE) }
+//
+//        if (v == navLogout) {
+//            logout()
+//            return
+//        }
+//
+//        val fragment = when (v) {
+//            navHome -> HomeFragment()
+//            navSchedule -> ScheduleFragment()
+//            navOutdoorMaps -> OutdoorMapsFragment()
+//            navIndoorMaps -> IndoorMapsFragment()
+//            navProfile -> ProfileFragment()
+//            navAdmin -> AdminFragment()
+//            navScanner -> ScannerFragment()
+//            else -> return
+//        }
+//
+//        v.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.selectedMenuItem))
+//        supportActionBar?.title = (v as TextView).text
+//
+//        supportFragmentManager.beginTransaction().replace(R.id.contentFrame, fragment).commit()
+//        drawerLayout.closeDrawer(GravityCompat.START)
+//    }
+//
+//    private fun updateUserInfo(user: User?) {
+//        user?.let {
+//            navMenu.nameTextView.text = it.fullName
+//            navMenu.emailTextView.text = it.email
+//        }
+//    }
+//
+//    /**
+//     * Modify the available options in the nav menu
+//     * Staff: Can access the scanner
+//     * Admins: Can access scanner and admin tools
+//     */
+//    private fun updateRoles(roles: Roles?) = roles?.let { roles ->
+//        var listOfRoles: List<String> = roles.roles
+//        if (listOfRoles.contains("Admin")) {
+//            navAdmin.visibility = View.VISIBLE
+//            navScanner.visibility = View.VISIBLE
+//        }
+//        if (listOfRoles.contains("Staff")) {
+//            navScanner.visibility = View.VISIBLE
+//        }
+//    }
+//
+//    private fun logout() {
+//        clearJWT()
+//
+//        thread {
+//            App.database.clearAllTables()
+//
+//            runOnUiThread {
+//                val loginIntent = Intent(this, LoginActivity::class.java)
+//                loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                startActivity(loginIntent)
+//                finish()
+//            }
+//        }
+//    }
+//
+//    private fun clearJWT() {
+//        val editor = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).edit()
+//        editor.putString("jwt", defaultToken)
+//        editor.apply()
+//    }
+//
+//    private fun updateDeviceToken() {
+//        val token = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).getString("firebaseToken", defaultToken)
+//                ?: defaultToken
+//        if (token != defaultToken) {
+//            thread {
+//                App.getAPI().sendUserToken(DeviceToken(token)).execute()
+//
+//                val editor = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).edit()
+//                editor.putString("firebaseToken", defaultToken)
+//                editor.apply()
+//            }
+//        }
+//    }
 }
