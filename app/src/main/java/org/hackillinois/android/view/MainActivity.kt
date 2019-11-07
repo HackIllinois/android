9342a38da30650314fbcc9117e21f0b32c715285
patrickfeltes <pj6444@gmail.com>
2019-11-06 21:32:09 -0600

    Remove nav menu code and logout

--- app/src/main/java/org/hackillinois/android/view/MainActivity.kt
+++ app/src/main/java/org/hackillinois/android/view/MainActivity.kt
@@ -1,29 +1,17 @@
 package org.hackillinois.android.view
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import android.content.Context
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
-import android.view.View
 import android.widget.ImageButton
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.layout_nav_menu.*
-import kotlinx.android.synthetic.main.layout_nav_menu.view.*
 import org.hackillinois.android.App
 import org.hackillinois.android.R
-import org.hackillinois.android.database.entity.Roles
-import org.hackillinois.android.database.entity.User
 import org.hackillinois.android.notifications.DeviceToken
-import org.hackillinois.android.view.admin.AdminFragment
 import org.hackillinois.android.view.home.HomeFragment
 import org.hackillinois.android.view.schedule.ScheduleFragment
 import org.hackillinois.android.viewmodel.MainViewModel
@@ -78,56 +66,10 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.init()
-        //viewModel.user.observe(this, Observer { updateUserInfo(it) })
-        //viewModel.roles.observe(this, Observer { updateRoles(it) })
 
         updateDeviceToken()
     }
 
-//    private fun updateUserInfo(user: User?) {
-//        user?.let {
-//            navMenu.nameTextView.text = it.fullName
-//            navMenu.emailTextView.text = it.email
-//        }
-//    }
-//
-//    /**
-//     * Modify the available options in the nav menu
-//     * Staff: Can access the scanner
-//     * Admins: Can access scanner and admin tools
-//     */
-//    private fun updateRoles(roles: Roles?) = roles?.let { roles ->
-//        var listOfRoles: List<String> = roles.roles
-//        if (listOfRoles.contains("Admin")) {
-//            navAdmin.visibility = View.VISIBLE
-//            navScanner.visibility = View.VISIBLE
-//        }
-//        if (listOfRoles.contains("Staff")) {
-//            navScanner.visibility = View.VISIBLE
-//        }
-//    }
-//
-//    private fun logout() {
-//        clearJWT()
-//
-//        thread {
-//            App.database.clearAllTables()
-//
-//            runOnUiThread {
-//                val loginIntent = Intent(this, LoginActivity::class.java)
-//                loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//                startActivity(loginIntent)
-//                finish()
-//            }
-//        }
-//    }
-//
-//    private fun clearJWT() {
-//        val editor = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).edit()
-//        editor.putString("jwt", defaultToken)
-//        editor.apply()
-//    }
-//
     private fun updateDeviceToken() {
         val token = applicationContext.getSharedPreferences(applicationContext.getString(R.string.authorization_pref_file_key), Context.MODE_PRIVATE).getString("firebaseToken", defaultToken)
                 ?: defaultToken
