054a45e4b2f7efaee1415a755fd92da34586eb91
Raghav01 <raghavsaini@outlook.com>
2019-11-24 01:29:18 -0500

    Mentor UI Updated

--- app/src/main/java/org/hackillinois/android/view/MentorFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/MentorFragment.kt
@@ -1,5 +1,16 @@
 package org.hackillinois.android.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.hackillinois.android.R
 
-class MentorFragment : Fragment()
+class MentorFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_mentor, container, false)
+
+        return view;
+    }
+}
