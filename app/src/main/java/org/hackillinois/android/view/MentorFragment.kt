0f79f327a5c6146341541b8cac7846faca419ac1
Raghav01 <raghavsaini@outlook.com>
2019-12-04 15:15:00 -0600

    Mentor Fragment set up to be integrated with API endpoints when available

--- app/src/main/java/org/hackillinois/android/view/MentorFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/MentorFragment.kt
@@ -5,12 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import org.hackillinois.android.R
+import org.hackillinois.android.model.mentor.MentorModel
 
 class MentorFragment : Fragment() {
+
+    private lateinit var mentorList: ArrayList<MentorModel>
+
+    private lateinit var mentorRecycler: RecyclerView
+
+    private lateinit var myAdapter: MentorAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_mentor, container, false)
 
-        return view;
+        return view
+    }
+
+    fun setUpRecycler() {
     }
 }
