e9a0b884acca88c23233e4e1d5ce126c1d9ad091
patrickfeltes <pj6444@gmail.com>
2020-01-18 10:28:06 -0600

    Delete unused files

--- app/src/main/java/org/hackillinois/android/view/home/EventsListViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package org.hackillinois.android.view.home
-
-import androidx.recyclerview.widget.RecyclerView
-import android.view.View
-
-class EventsListViewHolder(val view: View) : RecyclerView.ViewHolder(view)
