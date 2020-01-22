7e2e8d8ab2a8b22e5c1185726ac2d6525d96bb92
patrickfeltes <pj6444@gmail.com>
2020-01-22 09:56:10 -0600

    Centralize JWT management utility functions

---
+++ app/src/main/java/org/hackillinois/android/common/JWTUtilities.kt
@@ -0,0 +1,32 @@
+package org.hackillinois.android.common
+
+import android.content.Context
+import android.content.SharedPreferences
+import org.hackillinois.android.R
+
+object JWTUtilities {
+    const val DEFAULT_JWT = ""
+    const val JWT_PREF_KEY = "jwt"
+
+    fun readJWT(context: Context): String {
+        val sharedPreferences = getSharedPreferences(context)
+        return sharedPreferences.getString(JWT_PREF_KEY, DEFAULT_JWT) ?: DEFAULT_JWT
+    }
+
+    fun writeJWT(context: Context, jwt: String) {
+        val editor = getSharedPreferences(context).edit()
+        editor.putString("jwt", jwt)
+        editor.apply()
+    }
+
+    fun clearJWT(context: Context) {
+        val editor = getSharedPreferences(context).edit()
+        editor.putString("jwt", DEFAULT_JWT)
+        editor.apply()
+    }
+
+    private fun getSharedPreferences(context: Context): SharedPreferences {
+        val prefKey = context.applicationContext.getString(R.string.authorization_pref_file_key)
+        return context.getSharedPreferences(prefKey, Context.MODE_PRIVATE)
+    }
+}
