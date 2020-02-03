35d7a1a3ca3ad8bf20790bd21726c4d2a94c4b84
eshia <eshiafr2@illinois.edu>
2020-02-03 01:38:34 -0600

    Added changes for filtering events (unresolved commented code)

--- app/src/main/java/org/hackillinois/android/view/schedule/ScheduleFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/schedule/ScheduleFragment.kt
@@ -1,5 +1,6 @@
 package org.hackillinois.android.view.schedule
 
+import android.app.Application
 import android.os.Bundle
 import com.google.android.material.tabs.TabLayout
 import androidx.fragment.app.Fragment
@@ -7,15 +8,24 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.LiveData
 import kotlinx.android.synthetic.main.fragment_schedule.view.*
 import org.hackillinois.android.R
+import org.hackillinois.android.common.FavoritesManager
+import org.hackillinois.android.database.entity.Event
+import org.hackillinois.android.view.home.eventlist.EventClickListener
+import org.hackillinois.android.view.schedule.EventsAdapter.ViewHolder
+import org.hackillinois.android.viewmodel.EventInfoViewModel
 import org.hackillinois.android.viewmodel.ScheduleViewModel
 
-class ScheduleFragment : Fragment() {
+class ScheduleFragment(val app: Application) : Fragment() {
 
-    private lateinit var favoriteScheduleButton: Button
+    private lateinit var favoriteButton: Button
+
+    lateinit var event: LiveData<Event>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_schedule, container, false)
@@ -26,11 +36,13 @@ class ScheduleFragment : Fragment() {
         view.scheduleContainer.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(view.scheduleDays))
         view.scheduleDays.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(view.scheduleContainer))
 
-        favoriteScheduleButton = view.findViewById(R.id.favScheduleButton)
+        favoriteButton = view.findViewById(R.id.favScheduleButton)
 
-        favoriteScheduleButton.setOnClickListener(favScheduleClickListener)
+        favoriteButton.setOnClickListener(favScheduleClickListener)
 
         val scheduleViewModel = ScheduleViewModel()
+
+
 
         val time = System.currentTimeMillis()
 
@@ -49,9 +61,22 @@ class ScheduleFragment : Fragment() {
         override fun getCount() = 3
     }
 
-    private val favScheduleClickListener = View.OnClickListener {
-        favoriteScheduleButton.isSelected = true
 
 
+    private val favScheduleClickListener = OnClickListener {
+        favoriteButton.isSelected = true
+
+        //filterFavEvents(EventsAdapter.itemList)
+    }
+
+    private fun filterFavEvents(eventList: List<Event>): List<ScheduleListItem> {
+        eventList.forEach {
+            if (event.value?.name?.let { it1 -> FavoritesManager.isFavorited(app.applicationContext, it1) }!!) {
+                // keep in event list
+            } else {
+                // remove from event list
+            }
+        }
+        return eventList
     }
 }
