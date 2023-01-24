f40622fd9ce06ecfc1377f664afacb440078ef3c
Sunny Lee <sunnyjl2@illinois.edu>
2023-01-24 01:51:10 -0600

    popcorn fills entire screen, login button layout correct for big & small screens

--- app/src/main/java/org/hackillinois/android/view/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/hackillinois/android/view/onboarding/OnboardingActivity.kt
@@ -2,6 +2,9 @@ package org.hackillinois.android.view.onboarding
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Window
+import android.view.Window.FEATURE_NO_TITLE
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -41,6 +44,8 @@ class OnboardingActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(FEATURE_NO_TITLE)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_onboarding)
 
         view_pager.adapter = ScreenSlidePagerAdapter(this)
