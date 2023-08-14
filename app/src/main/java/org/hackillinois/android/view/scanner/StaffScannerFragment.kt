e1c1ef90d292bcc4275893364805b113cc965b30
Leah Ludwikowski <leahlud11@outlook.com>
2023-08-14 10:29:42 -0400

    Update StaffScannerFragment.kt

--- app/src/main/java/org/hackillinois/android/view/scanner/StaffScannerFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/scanner/StaffScannerFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import org.hackillinois.android.R
+import org.hackillinois.android.view.MainActivity
 
 class StaffScannerFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +19,22 @@ class StaffScannerFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_staff_scanner, container, false)
+
+        // for when meeting attendance button is clicked
+        val meetingAttendanceButton = view.findViewById<Button>(R.id.staffMeetingBtn)
+        meetingAttendanceButton.setOnClickListener {
+            val scannerFragment = ScannerFragment.newInstance("", "", true)
+            (context as MainActivity).switchFragment(scannerFragment, true)
+        }
+
+        // for when attendee check-in button is clicked
+        val staffCheckInButton = view.findViewById<Button>(R.id.staffCheckInBtn)
+        staffCheckInButton.setOnClickListener {
+            val scannerFragment = ScannerFragment.newInstance("", "", false)
+            (context as MainActivity).switchFragment(scannerFragment, true)
+        }
+
         return inflater.inflate(R.layout.fragment_staff_scanner, container, false)
     }
 }
