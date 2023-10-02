5203a5a755b58635290c3cf1eb590d85b74d3c92
Leah Ludwikowski <leahlud11@outlook.com>
2023-10-02 13:37:45 -0500

    Fixed ktlint issue

--- app/src/main/java/org/hackillinois/android/view/scanner/ScannerFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/scanner/ScannerFragment.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.transition.TransitionInflater
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
