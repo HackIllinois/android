e3e0ef08ecdd792b8f60d93638649e88271d0ade
Leah Ludwikowski <leahlud11@outlook.com>
2023-10-13 18:07:03 -0500

    fixed ktlint error

--- app/src/main/java/org/hackillinois/android/view/home/HomeFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
