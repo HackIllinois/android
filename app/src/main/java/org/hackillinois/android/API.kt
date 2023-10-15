c1c1ac4d9479779b1b0c912d131ff774867df1a2
Leah Ludwikowski <leahlud11@outlook.com>
2023-10-15 15:48:42 -0500

    removed unused profile/search/ endpoint

--- app/src/main/java/org/hackillinois/android/API.kt
+++ app/src/main/java/org/hackillinois/android/API.kt
@@ -59,8 +59,8 @@ interface API {
     @GET("profile/")
     suspend fun currentProfile(): Profile
 
-    @GET("profile/search/")
-    suspend fun allProfiles(): ProfileList
+//    @GET("profile/search/")
+//    suspend fun allProfiles(): ProfileList
 
     @GET("profile/leaderboard/?limit=10")
     suspend fun leaderboard(): LeaderboardList
