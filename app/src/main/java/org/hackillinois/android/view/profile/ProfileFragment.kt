fa73f03192636a7c0a49d547cb75151805f3a822
Aditya Kshirsagar <aditya29@icloud.com>
2023-10-24 21:58:34 -0500

    Removed unused imports.

--- app/src/main/java/org/hackillinois/android/view/profile/ProfileFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/profile/ProfileFragment.kt
@@ -7,7 +7,6 @@ import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
