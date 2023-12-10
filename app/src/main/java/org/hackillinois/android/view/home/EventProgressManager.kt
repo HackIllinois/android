33a6f3b5c89dd754aa9c2142ce1c1468595769a3
Leah Ludwikowski <leahlud11@outlook.com>
2023-12-10 13:17:18 -0600

    Added EventProgressManager to update background on home page

---
+++ app/src/main/java/org/hackillinois/android/view/home/EventProgressManager.kt
@@ -0,0 +1,103 @@
+package org.hackillinois.android.view.home
+
+import android.os.CountDownTimer
+import android.util.Log
+import org.hackillinois.android.R
+import org.hackillinois.android.common.isBeforeNow
+import org.hackillinois.android.common.timeUntilMs
+import java.util.*
+
+class EventProgressManager(val listener: CountDownListener) {
+
+    // CORRECT TIMES ARE NOT SET YET
+    //  2023-12-05 16:00:00
+    private val checkInTime: Calendar = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        timeInMillis = 1701813180000
+    }
+
+    // 2023-12-05 16:05:00
+    private val scavengerHuntTime: Calendar = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        timeInMillis = 1701813240000
+    }
+
+    // 2023-12-05 16:05:00
+    private val openingCeremonyTime: Calendar = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        timeInMillis = 1701813240000
+    }
+
+    // 2023-12-05 16:05:00
+    private val hackingTime: Calendar = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        timeInMillis = 1701813240000
+    }
+
+    // 2023-12-05 16:05:00
+    private val projectShowcaseTime: Calendar = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        timeInMillis = 1701813240000
+    }
+
+    // 2023-12-05 16:05:00
+    private val closingCeremonyTime: Calendar = Calendar.getInstance().apply {
+        timeZone = TimeZone.getTimeZone("America/Chicago")
+        timeInMillis = 1701813240000
+    }
+
+    private var times = listOf(checkInTime, scavengerHuntTime, openingCeremonyTime, hackingTime, projectShowcaseTime, closingCeremonyTime)
+    private var backgrounds = listOf(R.drawable.home_bg, R.drawable.fantasy_bg) // NEEDS TO BE UPDATED
+    private var timer: CountDownTimer? = null
+    private var state = 0
+
+    private val refreshRateMs = 500L
+
+    fun start() {
+        // find current state of the event progress
+        while (state < times.size && times[state].isBeforeNow()) {
+            state++
+        }
+        startTimer()
+    }
+
+    private fun startTimer() {
+        // if past the last event, don't start another timer
+        if (state >= times.size) {
+            listener.updateBackground(backgrounds[backgrounds.size - 1])
+            return
+        }
+
+        // else set the current title and start timer until next timestamp
+        listener.updateBackground(backgrounds[state])
+        val millisTillTimerFinishes = times[state].timeUntilMs()
+
+        timer = object : CountDownTimer(millisTillTimerFinishes, refreshRateMs) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("COUNTDOWN", "$millisUntilFinished")
+                Log.d("STATE", "$state")
+                // do nothing
+            }
+
+            override fun onFinish() {
+                state++
+                startTimer()
+            }
+        }.start()
+    }
+
+    fun onPause() {
+        timer?.cancel()
+        timer = null
+    }
+
+    fun onResume() {
+        if (timer == null) {
+            start()
+        }
+    }
+
+    interface CountDownListener {
+        fun updateBackground(newBackgroundResource: Int)
+    }
+}
--- app/src/main/java/org/hackillinois/android/view/home/HomeFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/home/HomeFragment.kt
@@ -1,25 +1,31 @@
 package org.hackillinois.android.view.home
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.load.resource.drawable.DrawableResource
 import org.hackillinois.android.R
 import org.hackillinois.android.common.TimeInfo
 import org.hackillinois.android.viewmodel.HomeViewModel
 
-class HomeFragment : Fragment(), CountdownManager.CountDownListener {
+class HomeFragment : Fragment(), CountdownManager.CountDownListener, EventProgressManager.CountDownListener {
 
     private lateinit var daysValue: TextView
     private lateinit var hoursValue: TextView
     private lateinit var minutesValue: TextView
     private lateinit var countdownTextView: TextView
+    private lateinit var homeBackgroundImageView: ImageView
 
     private lateinit var viewModel: HomeViewModel
-    private val countDownManager = CountdownManager(this)
+    private val countDownManager = CountdownManager(this) // for timer countdown
+    private val eventProgressManager = EventProgressManager(this) // for updating background of home page
 
     private var isActive = false
 
@@ -36,6 +42,7 @@ class HomeFragment : Fragment(), CountdownManager.CountDownListener {
         hoursValue = view.findViewById(R.id.hoursValue)
         minutesValue = view.findViewById(R.id.minutesValue)
         countdownTextView = view.findViewById(R.id.countdownTextView)
+        homeBackgroundImageView = view.findViewById(R.id.homeBackgroundImageView)
 
         return view
     }
@@ -44,18 +51,21 @@ class HomeFragment : Fragment(), CountdownManager.CountDownListener {
         super.onStart()
         isActive = true
         countDownManager.start()
+        eventProgressManager.start()
     }
 
     override fun onPause() {
         super.onPause()
         isActive = false
         countDownManager.onPause()
+        eventProgressManager.onPause()
     }
 
     override fun onResume() {
         super.onResume()
         isActive = true
         countDownManager.onResume()
+        eventProgressManager.onResume()
     }
 
     override fun onStop() {
@@ -76,6 +86,14 @@ class HomeFragment : Fragment(), CountdownManager.CountDownListener {
     override fun updateTitle(newTitle: String) {
         if (isActive) {
             countdownTextView.text = newTitle
+        }
+    }
+
+    override fun updateBackground(newBackgroundResource: Int) {
+        Log.d("IN HOME", "here")
+        if (isActive) {
+            Log.d("IS ACTIVE", "here")
+            homeBackgroundImageView.setImageResource(newBackgroundResource)
         }
     }
 
