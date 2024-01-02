9c402e2e5c4972ce21cb644d7d8468a0767af0f6
Leah Ludwikowski <leahlud11@outlook.com>
2024-01-02 17:06:55 -0500

    removed unused import

--- app/src/main/java/org/hackillinois/android/view/MainActivity.kt
+++ app/src/main/java/org/hackillinois/android/view/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.WindowManager
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
