d7dbfb54228cfecfb3148725ec5810db4c45f110
Sunny Lee <sunnyjl2@illinois.edu>
2024-01-17 21:06:53 -0600

    imageView Glide

--- app/src/main/java/org/hackillinois/android/view/shop/ShopAdapter.kt
+++ app/src/main/java/org/hackillinois/android/view/shop/ShopAdapter.kt
@@ -1,18 +1,15 @@
 package org.hackillinois.android.view.shop
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.shop_tile.view.*
 import org.hackillinois.android.R
 import org.hackillinois.android.database.entity.ShopItem
-import java.util.concurrent.Executors
 
 class ShopAdapter(private var itemList: List<ShopItem>) :
     RecyclerView.Adapter<ShopAdapter.ViewHolder>() {
@@ -49,29 +46,32 @@ class ShopAdapter(private var itemList: List<ShopItem>) :
             val quantity = item.quantity
             quantityTextView.text = resources.getString(R.string.shopquantity, quantity)
 
+            val imageURL = item.imageURL
+            val shopItemImageView: ImageView = itemView.findViewById(R.id.shopItemImageView)
+            Glide.with(context).load(imageURL).into(shopItemImageView)
+
             // Declaring executor to parse the URL
-            val executor = Executors.newSingleThreadExecutor()
+            // val executor = Executors.newSingleThreadExecutor()
             // Once the executor parses the URL and receives the image, handler will load it
             // in the ImageView
-            val handler = Handler(Looper.getMainLooper())
+            // val handler = Handler(Looper.getMainLooper())
             // Initializing the image
-            var image: Bitmap? = null
-            val imageURL = item.imageURL
-            executor.execute() {
-                try {
-                    val `in` = java.net.URL(imageURL).openStream()
-                    image = BitmapFactory.decodeStream(`in`)
+//            var image: Bitmap? = null
+//            val imageURL = item.imageURL
+//            executor.execute() {
+//                try {
+//                    val `in` = java.net.URL(imageURL).openStream()
+//                    image = BitmapFactory.decodeStream(`in`)
 
-                    // Only for making changes in UI
-                    handler.post {
-                        shopItemImageView.setImageBitmap(image)
-                    }
-                }
-                // If the URL doesnot point to image or any other kind of failure
-                catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
+            // Only for making changes in UI
+//                    handler.post {
+//                        shopItemImageView.setImageBitmap(image)
+//                    }
+//                }
+            // If the URL doesnot point to image or any other kind of failure
+//                catch (e: Exception) {
+//                    e.printStackTrace()
+//                }
         }
     }
 
