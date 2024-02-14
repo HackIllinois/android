0edb7012894f844962f3e453d960ff79761bc68f
Leah Ludwikowski <leahlud11@outlook.com>
2024-02-14 13:39:05 -0600

    Removed hardcoded shift data

--- app/src/main/java/org/hackillinois/android/repository/ShiftRepository.kt
+++ app/src/main/java/org/hackillinois/android/repository/ShiftRepository.kt
@@ -12,9 +12,6 @@ import org.hackillinois.android.model.event.ShiftsList
 class ShiftRepository {
     private val shiftDao = App.database.shiftDao()
 
-    // write fetchShifts() and refreshAll() functions
-    // look at other repository functions for reference (i.e. LeaderboardRepository)
-
     fun fetchShiftsHappeningBetweenTimes(startTime: Long, endTime: Long): LiveData<List<Shift>> {
         return shiftDao.getShiftsHappeningBetweenTimes(startTime / MILLIS_IN_SECOND, endTime / MILLIS_IN_SECOND)
     }
@@ -23,80 +20,9 @@ class ShiftRepository {
         // ensures database operation happens on the IO dispatcher
         withContext(Dispatchers.IO) {
             try {
-                // val shifts = App.getAPI().allShifts().shifts
-
-                val shifts = ShiftsList(
-                    listOf(
-                        Shift(
-                            eventId = "event 1",
-                            name = "test 1",
-                            description = "...",
-                            startTime = 1708734216,
-                            endTime = 1708907016,
-                            locations = listOf(
-                                EventLocation(
-                                    description = "SIEBEL",
-                                    tags = listOf("TAG1", "TAG2"),
-                                    latitude = 12.0,
-                                    longitude = -80.0
-                                )
-                            ),
-                            isAsync = false
-                        ),
-                        Shift(
-                            eventId = "event 2",
-                            name = "test 2",
-                            description = "...",
-                            startTime = 1708734216,
-                            endTime = 1708907016,
-                            locations = listOf(
-                                EventLocation(
-                                    description = "SIEBEL",
-                                    tags = listOf("TAG1", "TAG2"),
-                                    latitude = 12.0,
-                                    longitude = -80.0
-                                )
-                            ),
-                            isAsync = false
-                        ),
-                        Shift(
-                            eventId = "event 3",
-                            name = "test 3",
-                            description = "...",
-                            startTime = 1708739316,
-                            endTime = 1708739516,
-                            locations = listOf(
-                                EventLocation(
-                                    description = "SIEBEL",
-                                    tags = listOf("TAG1", "TAG2"),
-                                    latitude = 12.0,
-                                    longitude = -80.0
-                                )
-                            ),
-                            isAsync = false
-                        ),
-                        Shift(
-                            eventId = "event 4",
-                            name = "test 4",
-                            description = "...",
-                            startTime = 1708743000,
-                            endTime = 1708744000,
-                            locations = listOf(
-                                EventLocation(
-                                    description = "SIEBEL",
-                                    tags = listOf("TAG1", "TAG2"),
-                                    latitude = 12.0,
-                                    longitude = -80.0
-                                )
-                            ),
-                            isAsync = false
-                        )
-                        // Add more Shift objects as needed...
-                    )
-                )
-
+                 val shifts = App.getAPI().allShifts().shifts
                 Log.d("Shifts Fetched", shifts.toString())
-                shiftDao.clearTableAndInsertShifts(shifts.shifts)
+                shiftDao.clearTableAndInsertShifts(shifts)
             } catch (e: Exception) {
                 Log.e("REFRESH SHIFTS", e.toString())
             }
