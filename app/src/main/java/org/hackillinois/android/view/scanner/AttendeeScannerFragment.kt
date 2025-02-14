d380bd1d2c0dd87fb7e2ac682015d405bd05653e
Aditya Kshirsagar <aditya29@icloud.com>
2025-02-14 12:53:55 -0600

    Updated Scanner Layouts

--- app/src/main/java/org/hackillinois/android/view/scanner/AttendeeScannerFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/scanner/AttendeeScannerFragment.kt
@@ -42,15 +42,15 @@ class AttendeeScannerFragment : Fragment() {
         }
 
         // when point shop button is clicked
-        val pointShopButton = view.findViewById<Button>(R.id.pointShopBtn)
-        pointShopButton.setOnClickListener {
-            if (appBar != null && scannerBtn != null) {
-                appBar.visibility = View.INVISIBLE
-                scannerBtn.visibility = View.INVISIBLE
-            }
-            val scannerFragment = ScannerFragment.newInstance("point-shop")
-            (context as MainActivity).switchFragment(scannerFragment, true)
-        }
+//        val pointShopButton = view.findViewById<Button>(R.id.pointShopBtn)
+//        pointShopButton.setOnClickListener {
+//            if (appBar != null && scannerBtn != null) {
+//                appBar.visibility = View.INVISIBLE
+//                scannerBtn.visibility = View.INVISIBLE
+//            }
+//            val scannerFragment = ScannerFragment.newInstance("point-shop")
+//            (context as MainActivity).switchFragment(scannerFragment, true)
+//        }
 
         return view
     }
