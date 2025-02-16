b0658e84f5fb5349c205169969359bafe1a76499
Aryan-Bahl <abahl3@illinois.edu>
2025-02-16 13:02:05 -0600

    profile page finished

--- app/src/main/java/org/hackillinois/android/view/profile/ProfileFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/profile/ProfileFragment.kt
@@ -17,7 +17,6 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
-import kotlinx.android.synthetic.main.fragment_profile.profileBackground
 import org.hackillinois.android.R
 import org.hackillinois.android.common.JWTUtilities
 import org.hackillinois.android.database.entity.Profile
@@ -129,7 +128,7 @@ class ProfileFragment : Fragment() {
         avatarImage = view.findViewById(R.id.avatarImage)
         waveText = view.findViewById(R.id.waveText)
         attendeeTypeText = view.findViewById(R.id.attendeeTypeText)
-        rankingPlacementText = view.findViewById(R.id.rankingPlacementTextView)
+        rankingPlacementText = view.findViewById(R.id.rankValue)
 
         // Displays the logout button in the top-right corner if an attendee
         val logoutButton = view.findViewById<ImageButton>(R.id.logoutButton)
@@ -185,7 +184,7 @@ class ProfileFragment : Fragment() {
                 multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints)
 
             val clear = Color.TRANSPARENT
-            val solid = Color.parseColor("#662B13")
+            val solid = Color.parseColor("#000000")
             // creates qr code based on bitMatrix
             for (x in 0 until width) {
                 for (y in 0 until (height)) {
@@ -206,7 +205,8 @@ class ProfileFragment : Fragment() {
     private fun isStaff(): Boolean {
         val context = requireActivity().applicationContext
         val prefString = context.getString(R.string.authorization_pref_file_key)
-        return context.getSharedPreferences(prefString, Context.MODE_PRIVATE).getString("provider", "") ?: "" == "google"
+        return (context.getSharedPreferences(prefString, Context.MODE_PRIVATE)
+            .getString("provider", "") ?: "") == "google"
     }
 
     private fun isAttendee(): Boolean {
