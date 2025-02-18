20f297d5ad78a5918e5709d08e7e5f0f3fae6420
Aditya Kshirsagar <aditya29@icloud.com>
2025-02-17 20:47:35 -0600

    changed splashscreen and fixed variables on shop screen

--- app/src/main/java/org/hackillinois/android/view/shop/ShopFragment.kt
+++ app/src/main/java/org/hackillinois/android/view/shop/ShopFragment.kt
@@ -285,7 +285,7 @@ class ShopFragment : Fragment(), ShopAdapter.OnBuyItemListener {
 
     private fun updateCoinTotalUI(newProfile: Profile?) {
         if (newProfile != null) {
-            coin_total_textview.text = String.format("%,d", newProfile.pointsAccumulated)
+            number_of_coins_textview.text = String.format("%,d", newProfile.pointsAccumulated)
         }
     }
 
